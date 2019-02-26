614ef720ea6143eb820b6dbf819875b3498dcdf9
Claudio d'Angelis <claudiodangelis@gmail.com>
2019-02-26 17:59:28 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+A Chrome profile launcher [rofi](https://github.com/DaveDavenport/rofi).
+
+
+Requires `bash` and `python`.
+
+```sh
+rofi -modi chrome-profile:/path/to/rofi-chrome-profile-launcher.sh -show chrome-profile
+```
+
+## i3wm
+
+bindsym Mod1+Shift+Control+o exec rofi -modi chrome-profile:/path/to/rofi-chrome-profile-launcher/rofi-chrome-profile-launcher.sh -show chrome-profile
---
+++ rofi-chrome-profile-launcher.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+CHROME_VERSION=""
+CHROME_VERSIONS=(
+    "chromium"
+    "google-chrome"
+    "google-chrome-beta"
+    "google-chrome-unstable"
+)
+for version in "${CHROME_VERSIONS[@]}"; do
+    if [ -d "$HOME/.config/$version" ]; then
+        CHROME_VERSION="$version"
+        break
+    fi
+done
+if [ -z "$CHROME_VERSION" ]; then
+    echo "unable to find Chrome version"
+    exit 1
+fi
+CHROME_USER_DATA_DIR="$HOME/.config/$CHROME_VERSION"
+if [ ! -d "$CHROME_USER_DATA_DIR" ]; then
+    echo "unable to find Chrome user data dir"
+    exit 1
+fi
+DATA=$(python << END
+import json
+with open("$CHROME_USER_DATA_DIR/Local State") as f:
+    data = json.load(f)
+
+# print(data["profile"]["info_cache"])
+for profile in data["profile"]["info_cache"]:
+    print("%s_____%s" % (profile, data["profile"]["info_cache"][profile]["name"]))
+END
+)
+declare -A profiles=()
+while read -r line
+do
+    PROFILE="${line%_____*}"
+    NAME="${line#*_____}"
+    # echo "$PROFILE - $NAME"
+    profiles["$NAME"]="$PROFILE"
+done <<< "$DATA"
+
+if [ -z "$@" ]; then
+    for profile in "${!profiles[@]}"; do
+        echo $profile
+    done
+else
+    NAME="${@}"
+    $CHROME_VERSION --profile-directory="${profiles[$NAME]}" &> /dev/null
+fi
