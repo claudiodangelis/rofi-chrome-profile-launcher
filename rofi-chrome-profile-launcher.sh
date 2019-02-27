c2d7cac649d167111a6d632419bbfb11950b0c39
Claudio d'Angelis <claudiodangelis@gmail.com>
2019-02-27 09:31:33 +0100

    added comments

--- rofi-chrome-profile-launcher.sh
+++ rofi-chrome-profile-launcher.sh
@@ -2,14 +2,15 @@
 ## If you don't want the script to automatically choose the Chrome version to
 ## use, set the CHROME_VERSION variable below
 CHROME_VERSION=""
-CHROME_VERSIONS=(
-    "chromium"
-    "google-chrome"
-    "google-chrome-beta"
-    "google-chrome-unstable"
-)
-
+# Chrome version is not set, the script will try to locate it by looping through
+# all the possible chrome versions and checking if its user data dir exists
 if [ -z "$CHROME_VERSION" ]; then
+    CHROME_VERSIONS=(
+        "chromium"
+        "google-chrome"
+        "google-chrome-beta"
+        "google-chrome-unstable"
+    )
     for version in "${CHROME_VERSIONS[@]}"; do
         if [ -d "$HOME/.config/$version" ]; then
             CHROME_VERSION="$version"
@@ -17,18 +18,18 @@ if [ -z "$CHROME_VERSION" ]; then
         fi
     done
 fi
-
+# Chrome version was not set and it could not be automatically found either
 if [ -z "$CHROME_VERSION" ]; then
     echo "unable to find Chrome version"
     exit 1
 fi
-
+# Check if the user data dir actually exists
 CHROME_USER_DATA_DIR="$HOME/.config/$CHROME_VERSION"
 if [ ! -d "$CHROME_USER_DATA_DIR" ]; then
     echo "unable to find Chrome user data dir"
     exit 1
 fi
-
+# Run a python script to read profiles data from an state file used by Chrome
 DATA=$(python << END
 import json
 with open("$CHROME_USER_DATA_DIR/Local State") as f:
@@ -39,21 +40,21 @@ for profile in data["profile"]["info_cache"]:
     print("%s_____%s" % (profile, data["profile"]["info_cache"][profile]["name"]))
 END
 )
-
+# Populate an associative array that maps profiles names to directories
 declare -A profiles=()
 while read -r line
 do
     PROFILE="${line%_____*}"
     NAME="${line#*_____}"
-    # echo "$PROFILE - $NAME"
     profiles["$NAME"]="$PROFILE"
 done <<< "$DATA"
-
 if [ -z "$@" ]; then
+    # No argument passed, meaning that rofi was launched: show the profiles
     for profile in "${!profiles[@]}"; do
         echo $profile
     done
 else
+    # One argument passed, meaning that user selected a profile: launch Chrome
     NAME="${@}"
     $CHROME_VERSION --profile-directory="${profiles[$NAME]}" &> /dev/null
 fi
