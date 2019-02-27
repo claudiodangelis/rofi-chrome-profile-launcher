c8aa15ef61e4c5cd33442a47ffb52e936cd93975
Claudio d'Angelis <claudiodangelis@gmail.com>
2019-02-27 09:32:24 +0100

    spacing

--- rofi-chrome-profile-launcher.sh
+++ rofi-chrome-profile-launcher.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+
 ## If you don't want the script to automatically choose the Chrome version to
 ## use, set the CHROME_VERSION variable below
 CHROME_VERSION=""
+
 # Chrome version is not set, the script will try to locate it by looping through
 # all the possible chrome versions and checking if its user data dir exists
 if [ -z "$CHROME_VERSION" ]; then
@@ -18,17 +20,20 @@ if [ -z "$CHROME_VERSION" ]; then
         fi
     done
 fi
+
 # Chrome version was not set and it could not be automatically found either
 if [ -z "$CHROME_VERSION" ]; then
     echo "unable to find Chrome version"
     exit 1
 fi
+
 # Check if the user data dir actually exists
 CHROME_USER_DATA_DIR="$HOME/.config/$CHROME_VERSION"
 if [ ! -d "$CHROME_USER_DATA_DIR" ]; then
     echo "unable to find Chrome user data dir"
     exit 1
 fi
+
 # Run a python script to read profiles data from an state file used by Chrome
 DATA=$(python << END
 import json
@@ -40,6 +45,7 @@ for profile in data["profile"]["info_cache"]:
     print("%s_____%s" % (profile, data["profile"]["info_cache"][profile]["name"]))
 END
 )
+
 # Populate an associative array that maps profiles names to directories
 declare -A profiles=()
 while read -r line
@@ -48,6 +54,7 @@ do
     NAME="${line#*_____}"
     profiles["$NAME"]="$PROFILE"
 done <<< "$DATA"
+
 if [ -z "$@" ]; then
     # No argument passed, meaning that rofi was launched: show the profiles
     for profile in "${!profiles[@]}"; do
